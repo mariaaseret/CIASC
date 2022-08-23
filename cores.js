5bed88e2b6216ff657f8c08c3abdce1d1e95b8b1
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2022-08-23 17:39:48 -0300

    muda cor
    muda as cores do cubo no metaverso

---
+++ cores.js
@@ -0,0 +1,6 @@
+(function () {
+function randRange(low, high){
+return Math.floor(low + Math.random() * (high - low));
+}
+this.clickDownOnEntity = function (entity, mouseEvent) {
+Entities.editEntity(entityID, { color: {red: randRange(0,255), green: randRange(0,255), blue: randRange(0,255)} });
