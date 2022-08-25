ed46a2ea66a7191761d7c8456e2e50130ea6a395
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2022-08-25 15:41:37 -0300

    Create som.js
    ao clicar emite um som

---
+++ som.js
@@ -0,0 +1,42 @@
+(function() {
+    var myID; 
+    var myParentID;
+    var mySeatNumber;
+    var reset = false;
+    var RESET_TIME = 500;
+    var LOCATION_ROOT_URL = Script.resolvePath(".");
+    var clickSound = SoundCache.getSound(LOCATION_ROOT_URL + "448086__breviceps__normal-click.wav");    
+    
+    this.preload = function (entityID) {
+        myID = entityID;
+        myParentID = Entities.getEntityProperties(myID,["parentID"]).parentID;
+        mySeatNumber = Entities.getEntityProperties(myID,["description"]).description;     
+    };
+
+    function click() {
+        var user = Account.username;       
+        if (reset) {
+            var injectorOptions = {
+                position: MyAvatar.position,
+                volume: 1,
+                localOnly: true            
+            };
+            Audio.playSound(clickSound, injectorOptions);   
+            print(user + "start is Clicked!");           
+            Entities.callEntityServerMethod(                             
+                myParentID, 
+                "registerUser",
+                [MyAvatar.sessionUUID,MyAvatar.displayName,user,mySeatNumber]
+            );            
+            reset = false;
+        }         
+    }
+
+    Script.setInterval(function () {
+        reset = true;    
+    }, RESET_TIME);
+
+    this.startNearTrigger = click;
+    this.clickDownOnEntity = click;
+    this.startFarTrigger = click;
+});
