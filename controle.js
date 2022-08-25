16787554f37ecbea721ab9d9bcd30c1528be4604
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2022-08-25 14:41:46 -0300

    Update controle.js

--- controle.js
+++ controle.js
@@ -4,7 +4,7 @@
     }
 
     this.clickDownOnEntity = function (entityID, mouseEvent) {
-        Entities.editEntity({0b109ebe-4a97-4a85-91f2-ccd5354e79ce}, { color: { red: randRange(0,255), green: randRange(0,255), blue: randRange(0,255)} });
+        Entities.editEntity('0b109ebe-4a97-4a85-91f2-ccd5354e79ce', { color: { red: randRange(0,255), green: randRange(0,255), blue: randRange(0,255)} });
     };
 })
 
