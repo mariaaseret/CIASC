48e55aaa564b4f0f6a10d83bf7563a39019471ce
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2022-09-27 18:10:15 -0300

    Update porta.js

--- porta.js
+++ porta.js
@@ -11,11 +11,8 @@
     
 
     this.clickDownOnEntity = function (entityID, mouseEvent) {
-     
       Script.setInterval(function () {
-        Entities.editEntity(entityID, { 
-            color: { red: randRange(0,255), green: randRange(0,255), blue: randRange(0,255)} },
-            dimensions: { x: rangeDoor(0,5), y: 0.5, z: 0.5 },);
+        Entities.editEntity(entityID, { color: { red: randRange(0,255), green: randRange(0,255), blue: randRange(0,255)} }, dimensions: { x: rangeDoor(0,5), y: 0.5, z: 0.5 });
       }, 1000);
 
       Script.setTimeout(function () {
