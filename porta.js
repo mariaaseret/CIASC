eba4a8158a723deb7d89c4028af1c2405858c73f
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2022-09-27 19:32:28 -0300

    Create porta.js

---
+++ porta.js
@@ -0,0 +1,18 @@
+(function () {
+    function randRange(low, high) {
+        return Math.floor(low + Math.random() * (high - low));
+    }
+    function rangeDoor(low, high) {
+        for(var i = 0; i < high; ++i){
+                return count++;
+         }   
+    }
+
+
+    this.clickDownOnEntity = function (entityID, mouseEvent) {
+         for(var i = 0; i < 10; ++i){
+            Entities.editEntity(entityID, { dimensions: { x: i, y: 15, z: 7 } });
+         }
+        
+    };
+})
