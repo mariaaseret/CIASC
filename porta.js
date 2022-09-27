90900336f10b3f21f6c1d1c35e13ea0c4afe4709
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2022-09-27 17:54:11 -0300

    Update porta.js

--- porta.js
+++ porta.js
@@ -7,9 +7,9 @@
         for(var i = 0; i < array.length; ++i){
             if(array[i] == 2)
                 return count++;
-            }   
-        }
+         }   
     }
+    
 
     this.clickDownOnEntity = function (entityID, mouseEvent) {
      
