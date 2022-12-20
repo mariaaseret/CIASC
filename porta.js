74a11edd7934048fa9deb043288f54562a33d5d3
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2022-12-20 15:33:32 -0300

    Update porta.js

--- porta.js
+++ porta.js
@@ -11,19 +11,19 @@
     
     this.clickDownOnEntity = function (entityID, mouseEvent) {
              Script.setTimeout(function () {
-                Entities.editEntity(entityID, { dimensions: { x: 1.9900, y: 5, z: 0.1895 } });
+                Entities.editEntity(entityID, { dimensions: { x: 18.0000, y: 26.0000, z: 0.0010 } });
             }, 1000);
             Script.setTimeout(function () {
-                Entities.editEntity(entityID, { dimensions: { x: 1.9900, y: 4, z: 0.1895 } });
+                Entities.editEntity(entityID, { dimensions: {x: 18.0000, y: 16.0000, z: 0.0010} });
             }, 2000);
             Script.setTimeout(function () {
-                Entities.editEntity(entityID, { dimensions: { x: 1.9900, y: 3, z: 0.1895 } });
+                Entities.editEntity(entityID, { dimensions: { x: 1.9900, y: 10.0000, z: 0.1895 } });
             }, 3000);
             Script.setTimeout(function () {
-                Entities.editEntity(entityID, { dimensions: { x: 1.9900, y: 4, z: 0.1895 } });
+                Entities.editEntity(entityID, { dimensions: { x: 1.9900, y: 5.0000, z: 0.1895 } });
             }, 5000);
             Script.setTimeout(function () {
-                Entities.editEntity(entityID, { dimensions: { x: 1.9900, y: 5, z: 0.1895 } });
+                Entities.editEntity(entityID, { dimensions: { x: 1.9900, y: 1.0000, z: 0.1895 } });
             }, 6000);
     };
 })
