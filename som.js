90ddf7e518c2ee509ad37ccf8914c76777a595b9
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2023-01-18 18:11:31 -0300

    Update som.js

--- som.js
+++ som.js
@@ -1,44 +1,13 @@
-//
-//  som.js
-//  
-//  Created by Basinsky on 24 Feb 2021
-//  
-//  Register button script for hearts game  
-//
-//  Distributed under the Apache License, Version 2.0.
-//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
-//
-(function() {
-    var myID; 
-    var myParentID;
-    var mySeatNumber;
-    var reset = false;
-    var RESET_TIME = 500;
-    var LOCATION_ROOT_URL = Script.resolvePath(".");
+
+(function () {
     var clickSound = SoundCache.getSound(LOCATION_ROOT_URL + "448086__breviceps__normal-click.wav");    
     
-  
-
-    function click() {
-        var user = Account.username;       
-        if (reset) {
-            var injectorOptions = {
-                position: MyAvatar.position,
-                volume: 1,
-                localOnly: true            
-            };
-            Audio.playSound(clickSound, injectorOptions);   
-            print(user + "start is Clicked!");           
-                   
-            reset = false;
-        }         
+    function randRange(low, high) {
+        return Math.floor(low + Math.random() * (high - low));
     }
 
-    Script.setInterval(function () {
-        reset = true;    
-    }, RESET_TIME);
-
-    this.startNearTrigger = click;
-    this.clickDownOnEntity = click;
-    this.startFarTrigger = click;
-});
+    this.clickDownOnEntity = function (entityID, mouseEvent) {
+        Entities.editEntity(entityID, { color: { red: randRange(0,255), green: randRange(0,255), blue: randRange(0,255)} });
+        Audio.playSound(clickSound);   
+    };
+})
