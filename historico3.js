09e81b72456f606fc937f9eeedb91f64cfdb47a4
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2023-01-24 18:10:39 -0300

    Update historico3.js

--- historico3.js
+++ historico3.js
@@ -1,12 +1,11 @@
 (function() {
-    var injector = 0;
     var myID; 
     var myParentID;
     var mySeatNumber;
     var reset = false;
-    var RESET_TIME = 500;
+    var RESET_TIME = 9000;
     var LOCATION_ROOT_URL = Script.resolvePath(".");
-    var somhistorico2 = SoundCache.getSound(LOCATION_ROOT_URL + "historico3.wav");    
+    var somhistorico3 = SoundCache.getSound(LOCATION_ROOT_URL + "historico3.wav");    
     
     this.preload = function (entityID) {
         myID = entityID;
@@ -15,11 +14,22 @@
     };
 
     function click() {
-            injector = Audio.playSound(somhistorico2, injectorOptions);
-            injector.restart();
+        var user = Account.username;       
+        if (reset) {
+            var injectorOptions = {
+                position: MyAvatar.position,
+                volume: 1,
+                localOnly: true            
+            };
+            Audio.playSound(somhistorico3, injectorOptions);         
+            reset = false;
+        }         
     }
 
-  
+    Script.setInterval(function () {
+        reset = true;    
+    }, RESET_TIME);
+
     this.startNearTrigger = click;
     this.clickDownOnEntity = click;
     this.startFarTrigger = click;
