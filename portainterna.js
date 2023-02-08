bc3396426f1c8a0dd9a975fb99cf2e14a6fffd15
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2023-02-08 18:36:31 -0300

    Create portainterna.js

---
+++ portainterna.js
@@ -0,0 +1,74 @@
+(function () {
+    function randRange(low, high) {
+        return Math.floor(low + Math.random() * (high - low));
+    }
+    function rangeDoor(low, high) {
+        for(var i = 0; i < high; ++i){
+                return count++;
+         }   
+    }
+    
+    
+    this.clickDownOnEntity = function (entityID, mouseEvent) {
+             Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: { x: 18.0000, y: 26.0000, z: 0.0010 } });
+            }, 1000);
+             Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 23.5000, z: 0.0010} });
+            }, 1250);
+             Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 21.0000, z: 0.0010} });
+            }, 1500);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 16.0000, z: 0.0010} });
+            }, 2000);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 13.5000, z: 0.0010} });
+            }, 2250);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 11.0000, z: 0.0010} });
+            }, 2500);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: { x: 18.0000, y: 6.0000, z: 0.0010 } });
+            }, 3000);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 5.0000, z: 0.0010} });
+            }, 3250);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: { x: 18.0000, y: 3.0000, z: 0.0010 } });
+            }, 3500);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: { x: 18.0000, y: 1.0000, z: 0.0010 } });
+            }, 4000);
+        
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: { x: 18.0000, y: 3.0000, z: 0.0010 } });
+            }, 25000);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 5.0000, z: 0.0010} });
+            }, 26000);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 6.0000, z: 0.0010} });
+            }, 27000);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 11.0000, z: 0.0010} });
+            }, 28000);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 13.5000, z: 0.0010} });
+            }, 28250);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 16.0000, z: 0.0010} });
+            }, 29500);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 21.0000, z: 0.0010} });
+            }, 30000);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: {x: 18.0000, y: 23.5000, z: 0.0010} });
+            }, 30250);
+            Script.setTimeout(function () {
+                Entities.editEntity('fabbdd48-452d-479e-aa88-293568043470', { dimensions: { x: 18.0000, y: 26.0000, z: 0.0010 } });
+            }, 30500);
+        
+        
+    };
+})
