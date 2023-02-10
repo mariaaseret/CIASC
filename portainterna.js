ec56e0dce63526c4712955e890d982e246017407
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2023-02-10 20:28:19 -0300

    Update portainterna.js

--- portainterna.js
+++ portainterna.js
@@ -108,13 +108,9 @@
                 Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.1000, y: -4.5093, z: -79.3065 }});
             }, 27500);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.7220, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.7219, y: -4.5093, z: -79.3065 }});
                 Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -4.9851, y: -4.5093, z: -79.3065 }});
-            }, 27600);
-            Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.7220, y: -4.5093, z: -79.3065 }});
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -4.9851, y: -4.5093, z: -79.3065 }});
-            }, 27780);
+            }, 27750);
         
     };
 })
