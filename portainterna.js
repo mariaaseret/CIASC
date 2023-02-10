753d2108a60f5a462b90700b05fcd9cd96db3423
Maria Teresa <37987077+mariaaseret@users.noreply.github.com>
2023-02-10 20:13:03 -0300

    Update portainterna.js

--- portainterna.js
+++ portainterna.js
@@ -11,106 +11,106 @@
     
     this.clickDownOnEntity = function (entityID, mouseEvent) {
              Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.7219, y: -4.5093, z: -79.3065 } });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -4.9851, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.7219, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -4.9851, y: -4.5093, z: -79.3065 }});
             }, 1000);
              Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.6000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.0000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.6000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.0000, y: -4.5093, z: -79.3065 }});
             }, 1250);
              Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.5000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.1000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.5000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.1000, y: -4.5093, z: -79.3065 }});
             }, 1500);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.4000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.2000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.4000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.2000, y: -4.5093, z: -79.3065 }});
             }, 2000);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.3000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.3000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.3000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.3000, y: -4.5093, z: -79.3065 }});
             }, 2250);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.2000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.4000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.2000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.4000, y: -4.5093, z: -79.3065 }});
             }, 2500);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.1000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.5000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.1000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.5000, y: -4.5093, z: -79.3065 }});
             }, 3000);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.0000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.6000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.0000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.6000, y: -4.5093, z: -79.3065 }});
             }, 3250);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -2.9000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.7000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -2.9000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.7000, y: -4.5093, z: -79.3065 }});
             }, 3500);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -2.8000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.8000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -2.8000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.8000, y: -4.5093, z: -79.3065 }});
             }, 4000);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -2.7000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.9000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -2.7000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.9000, y: -4.5093, z: -79.3065 }});
             }, 4250);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -2.6000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -6.0050, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -2.6000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -6.0050, y: -4.5093, z: -79.3065 }});
             }, 4500);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -2.5000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -6.2000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -2.5000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -6.2000, y: -4.5093, z: -79.3065 }});
             }, 4750);
         
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -2.5000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -6.2000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -2.5000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -6.2000, y: -4.5093, z: -79.3065 }});
             }, 25000);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -2.6000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -6.0050, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -2.6000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -6.0050, y: -4.5093, z: -79.3065 }});
             }, 25250);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -2.7000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.9000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -2.7000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.9000, y: -4.5093, z: -79.3065 }});
             }, 25500);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -2.8000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.8000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -2.8000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.8000, y: -4.5093, z: -79.3065 }});
             }, 25750);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -2.9000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.7000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -2.9000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.7000, y: -4.5093, z: -79.3065 }});
             }, 26000);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.0000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.6000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.0000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.6000, y: -4.5093, z: -79.3065 }});
             }, 26250);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.1000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.5000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.1000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.5000, y: -4.5093, z: -79.3065 }});
             }, 26500);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.2000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.4000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: {x: -3.2000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.4000, y: -4.5093, z: -79.3065 }});
             }, 26750);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.3000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.3000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.3000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.3000, y: -4.5093, z: -79.3065 }});
             }, 27000);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.4000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.2000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.4000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.2000, y: -4.5093, z: -79.3065 }});
             }, 27250);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.6000, y: -4.5093, z: -79.3065} });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.1000, y: -4.5093, z: -79.3065 } });
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.6000, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -5.1000, y: -4.5093, z: -79.3065 }});
             }, 27500);
             Script.setTimeout(function () {
-                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.7219, y: -4.5093, z: -79.3065 } });
-                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -4.9851, y: -4.5093, z: -79.3065 } });
-            }, 27750);
+                Entities.editEntity('3f002457-f1be-42b6-a436-4f663d232594', { localPosition: { x: -3.7220, y: -4.5093, z: -79.3065 }});
+                Entities.editEntity('fec9e9e7-e0e3-4bad-b982-a4cc7a22d08d', { localPosition: { x: -4.9851, y: -4.5093, z: -79.3065 }});
+            }, 27780);
         
     };
 })
